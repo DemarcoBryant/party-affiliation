f4138852399a3f83f84edd786ab943818fec3b04
DemarcoBryant <158539299+DemarcoBryant@users.noreply.github.com>
2024-02-12 13:16:25 -0500

    party affiliation

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Are you Democrat, Republican, or Independent?");
+        System.out.println("D - Democrat");
+        System.out.println("R - Republican");
+        System.out.println("I - Independent");
+        System.out.println("Enter your choice: ");
+        String party = scan.nextLine();
+        if (party.equalsIgnoreCase("D")) {
+            System.out.println("You get a Democratic Donkey");
+        } else if (party.equalsIgnoreCase("R")) {
+            System.out.println("You get a Republican Elephant");
+        } else if (party.equalsIgnoreCase("I")) {
+            System.out.println("You get a Independent Person");
+        } else {
+            System.out.println("Other choice made");
+        }
+    }
+}
